ace567eaa9b0613765e02b14638789030629c849
alex kalina <walterscott404@yandex.com>
2017-10-25 12:58:29 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -11,10 +11,17 @@ DBNAME = "tvshows"
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
+
 connection_details = YAML::load(File.open('config/database.yml'))
 
 # DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
 DB = ActiveRecord::Base.establish_connection(connection_details)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+
+require_all 'app'
 
 if ENV["ACTIVE_RECORD_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
